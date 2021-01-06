b2a819a1670cbf2dd6c6609efba67bd36f21065a
deepanshu <deepanshu>
2021-01-06 22:37:42 +0530

    reverse the number

---
+++ reverse.cpp
@@ -0,0 +1,25 @@
+
+
+#include<iostream>
+using namespace std;
+int main(){
+while(1){
+
+int num,rev=0 , rem=0;
+cout<<"enter number"<<endl;
+cin>>num;
+while(num > 0){
+
+    rem = num%10;
+    cout<<rem;
+    num = num/10;
+
+
+
+}
+cout<<"\n\n";
+
+
+
+}
+}
